b22f92e962c06e83ba3c52c095f76de8850567be
dustinhendrickson <dustin.hendrickson@gmail.com>
2013-04-18 08:35:56 -0500

    Removed old blog code

--- blog/adm/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-	blog <?=VERSION?>
-</div>
-</body>
-</html>
--- views/blogadmin.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (Functions::Verify_Session() == true){
-	header( 'Location: blog/adm/admin.php?mode=login&password=cool' );
-} else {
-	Functions::Verify_Session_Redirect();
-}
