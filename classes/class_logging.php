393202c6a428abcb71d73ae4e3f29cb8911a7c77
dustinhendrickson <dustin.hendrickson@gmail.com>
2013-04-30 00:00:14 -0500

    fixed path issue

--- classes/class_logging.php
+++ classes/class_logging.php
@@ -1,12 +1,13 @@
 <?php
 //Writes to a specified LOG file.
-function Write_Log($log,$logentry,$logpath="logs/",$showIP='True')
+function Write_Log($log, $logentry, $logpath='logs/', $showIP='True')
 {
     $NOW = date("h:ia m-d-Y");
+    $logpath = $_SERVER['DOCUMENT_ROOT'] . "/" . $logpath;
 
     if($showIP='True') { $ip = $_SERVER['REMOTE_ADDR']; } else { $ip = "System Process"; }
 
-    if (!file_exists($logpath.$log.'.log')){fopen($logpath . $log . '.log', 'w');}
+    if (!file_exists($logpath.$log.'.log')){fopen($logpath . $log . '.log', 'w+');}
 
     $logfile = fopen($logpath . $log . '.log', 'a');
 
