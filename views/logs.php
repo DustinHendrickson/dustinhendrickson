6e426afe9b90e9da1ed56d115ecf23ee04f29f8f
Dustin Hendickson <dustin@timeips.com>
2013-05-16 16:33:45 -0500

    Quick Test

--- views/logs.php
+++ views/logs.php
@@ -5,16 +5,17 @@ $log = $_GET['log'];
 $logFiles = scandir($GLOBALS['Path'] . '/logs/');
 
 foreach($logFiles as $file) {
-    if ($file != '.' && $file != '..')
-    echo "<a href='?view=logs&log={$file}'>";
-    echo $file;
-    echo "</a> |";
+    if ($file != '.' && $file != '..') {
+        echo "<a href='?view=logs&log={$file}'>";
+        echo $file;
+        echo "</a> |";
+    }
 }
 
 echo '<br/><br/>';
 
 if (isset($log)) {
-    $logText = file_get_contents($log);
+    $logText = file_get_contents($GLOBALS['Path'] . '/logs/' . $log);
 
     echo $logText;
 }
