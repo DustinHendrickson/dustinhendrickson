5edae5ca3c677401277b2effc685f8560404985c
DustinHendrickson <dustin.hendrickson@gmail.com>
2015-07-01 17:52:28 -0500

    Added preload to images

--- views/petbattle_storymode.php
+++ views/petbattle_storymode.php
@@ -1,4 +1,13 @@
 <?php unset($_SESSION['Story_Mode']); ?>
+<div id="Preload" style="display: none;">
+    <img src="petbattles/images/tiles/PetHospital.png" width="1" height="1" alt="preload" />
+    <img src="petbattles/images/tiles/Rock.png" width="1" height="1" alt="preload" />
+    <img src="petbattles/images/tiles/TallGrass.png" width="1" height="1" alt="preload" />
+    <img src="petbattles/images/tiles/Trainer1Down.png" width="1" height="1" alt="preload" />
+    <img src="petbattles/images/tiles/Trainer1Up.png" width="1" height="1" alt="preload" />
+    <img src="petbattles/images/tiles/Trainer1Left.png" width="1" height="1" alt="preload" />
+    <img src="petbattles/images/tiles/Trainer1Right.png" width="1" height="1" alt="preload" />
+</div>
 <div class='ContentHeader'>Story Mode <a href='?view=petbattle_home'><img align='right' height='35' width='100' src='img/back.png'></a></div><br><hr><br>
 <center>
 <canvas id="canvasbg" style="position:absolute; z-index: 1; left:9%; top:12%;" width="800px" height="480px"></canvas>
