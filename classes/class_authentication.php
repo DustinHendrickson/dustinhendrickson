74bcc9c068c77a56c8715c42a5381bec4476a087
candyapplecorn <candyapplecorn@gmail.com>
2015-10-09 13:00:20 -0700

    Update class_authentication.php
    The login function is indented like crazy and it makes it a little hard to understand what's going on at first glance. Rather than doing
    "if (true) { stuff; } else { error_message; }"
    I'd prefer it if it was:
    "if (!true) { error_message; return; }"
    with the case of a successful login being @ the bottom after all other checks have run.
    
    This just makes indentation less rampant
    
    also this is my first fork, ive never used github before

--- classes/class_authentication.php
+++ classes/class_authentication.php
@@ -16,47 +16,47 @@ function Login($User,$Pass)
     $Password = Functions::Make_Safe($Pass);
 
     // Check if parameters are blank
-    if($Username!='' && $Password!='') {
-
-            // Query to get the credentials of the user logging in.
-            $Login_Array = array(':Username'=>$Username);
-            $Login_Result = $this->Connection->Custom_Query("SELECT * FROM users WHERE Username=:Username", $Login_Array);
-
-            // Check and see if there were any matches in the result.
-            if (!empty($Login_Result) && password_verify($Password, $Login_Result['Password'])) {
-
-                    //Check to see if the account is locked.
-                    if($Login_Result['Account_Locked'] == 0){
-
-                        //Login is successfully, now we set up our session variables.
-                        $_SESSION["Name"] = $Login_Result["Username"];
-                        $_SESSION["ID"] = $Login_Result["ID"];
-
-                        //Update the DB for the date of the login.
-                        $Last_Login_Array = array(':Account_Last_Login'=>date(self::DATE_FORMAT),':ID'=>$_SESSION['ID']);
-                        $this->Connection->Custom_Execute("UPDATE users SET Account_Last_Login=:Account_Last_Login WHERE ID=:ID",$Last_Login_Array);
-                        Write_Log("users", "ACCOUNT: Successfull login attempt for account [$Username]");
-
-                        // Display success toast.
-                        Toasts::addNewToast("You have logged in as " . $_SESSION['Name'], "success");
-                        $User = new User($_SESSION["ID"]);
-                        $User->Add_Achievement("Logged In");
-
-                        // Redirect user to homepage.
-                        header( 'Location: /');
-                        exit();
-                    } else {
-                        Toasts::addNewToast("This account is locked and may not log in.", "error");
-                        Write_Log("users", "ACCOUNT: Locked login attempt for account [$Username]");
-                    }
-            } else {
-                Toasts::addNewToast("Username and Password combination is incorrect.", "error");
-                Write_Log("users", "ACCOUNT: Failed login attempt for account [$Username]");
-            }
-    } else {
+    if(!($Username!='' && $Password!='')) {
         Toasts::addNewToast("Please fill out all required fields.", "error");
         Write_Log("users", "ACCOUNT: Not all login fields given.");
+        return;
     }
+
+    // Query to get the credentials of the user logging in.
+    $Login_Array = array(':Username'=>$Username);
+    $Login_Result = $this->Connection->Custom_Query("SELECT * FROM users WHERE Username=:Username", $Login_Array);
+
+    // Check and see if there were any matches in the result.
+    if (!(!empty($Login_Result) && password_verify($Password, $Login_Result['Password']))) {
+        Toasts::addNewToast("Username and Password combination is incorrect.", "error");
+        Write_Log("users", "ACCOUNT: Failed login attempt for account [$Username]");
+        return;
+    }
+
+    //Check to see if the account is locked.
+    if($Login_Result['Account_Locked'] != 0) {
+        Toasts::addNewToast("This account is locked and may not log in.", "error");
+        Write_Log("users", "ACCOUNT: Locked login attempt for account [$Username]");
+        return;                    
+    }
+
+    //Login is successfully, now we set up our session variables.
+    $_SESSION["Name"] = $Login_Result["Username"];
+    $_SESSION["ID"] = $Login_Result["ID"];
+
+    //Update the DB for the date of the login.
+    $Last_Login_Array = array(':Account_Last_Login'=>date(self::DATE_FORMAT),':ID'=>$_SESSION['ID']);
+    $this->Connection->Custom_Execute("UPDATE users SET Account_Last_Login=:Account_Last_Login WHERE ID=:ID",$Last_Login_Array);
+    Write_Log("users", "ACCOUNT: Successfull login attempt for account [$Username]");
+
+    // Display success toast.
+    Toasts::addNewToast("You have logged in as " . $_SESSION['Name'], "success");
+    $User = new User($_SESSION["ID"]);
+    $User->Add_Achievement("Logged In");
+
+    // Redirect user to homepage.
+    header( 'Location: /');
+    exit();
 }
 
 function Register($User,$Pass,$Mail,$Permissions='4')
